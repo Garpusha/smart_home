c54b796686baefb3d424910386a4bdf380717566
Garpusha <theflash@yandex.ru>
2023-08-25 13:09:36 +0300

    first_commit

---
+++ measurement/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+class Sensor(models.Model):
+    name = models.CharField(max_length=20, verbose_name="Наименование датчика")
+    description = models.CharField(
+        max_length=50, blank=True, verbose_name="Описание датчика"
+    )
+
+
+class Measurement(models.Model):
+    sensor = models.ForeignKey(
+        Sensor,
+        on_delete=models.CASCADE,
+        related_name="measurements",
+        verbose_name="ID датчика",
+    )
+    temperature = models.FloatField(verbose_name="Температура")
+    created_at = models.DateTimeField(auto_now_add=True)
